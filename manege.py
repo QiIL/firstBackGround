66cc2bbfff4ac99b1b1b40c85cdd98bdab01eae3
QiIL <qsy_skx_code@qq.com>
2017-05-15 16:14:55 +0800

    finish the relationship

--- manege.py
+++ manege.py
@@ -1,9 +1,18 @@
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from flask_sqlalchemy import SQLAlchemy
 from main import app, User, session, Base, Post,Comment,Tag
 from config import DevConfig
 
+
 manager = Manager(app)
 manager.add_command('server', Server())
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
